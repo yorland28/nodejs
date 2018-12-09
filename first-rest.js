2933d0ef9a7e10f64afb378b06558bea1d4d6f41
ygarcia <yorland28@gmail.com>
2018-12-09 17:51:25 -0300

    it was included the first API rest with express framework

---
+++ first-rest.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const status_ok = 200;
+
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/hello/:name',(rq,rs) => {
+  rs.status(status_ok).send({ result : `Hola ${rq.params.name}`});
+});
+
+app.post('/hello',(rq,rs) => {
+  rs.status(status_ok).send({ result : `Hola ${rq.body.name}`});
+});
+
+app.listen(port, () => {
+  console.log('Node Endpoints working: '+port);
+});
